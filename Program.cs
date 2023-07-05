be964e978e5d763d7706aa18f675c3d5ec4e3305
Roman <gawrysha@yandex.ru>
2023-07-06 01:16:52 +0500

    четвёртый

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string [] arraySum = new string [5] { "123", "1567", "-2", "computer science", ""};
+
+string [] arrayResult = new string[arraySum.Length];
+
+
+    string[] arrayFill(string[] arraySum, string[] arrayResult) {
+
+    int temp = 0;
+
+    for (int i = 0; i <arraySum.Length; i++)
+    {
+        if(arraySum[i].Length <=3) {
+
+            arrayResult[temp] = arraySum[i];
+            temp++;
+        }        
+    }
+    return arrayResult;
+}
+
+void arrayPrint (string [] array) {
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+arrayResult = arrayFill(arraySum, arrayResult);
+
+arrayPrint(arrayResult);
